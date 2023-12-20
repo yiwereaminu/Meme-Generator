ee8ca3fa347c5382fb34d5f0a78c9464d68a3bf7
yiwereaminu <yiwereamin@gmail.com>
2023-12-20 22:25:32 +0000

    done with markup

--- src/component/MainContent.jsx
+++ src/component/MainContent.jsx
@@ -1,22 +1,29 @@
-import { Form, Row, Col, Button } from "react-bootstrap";
+import { Form, Row, Col, Button, Container } from "react-bootstrap";
 
 const MainContent = () => {
   return (
-    <Form className="form-content">
-      <Row className="mb-3">
-        <Col>
-          <Form.Label className="text-center">Top Text</Form.Label>
-          <Form.Control type="text" placeholder="Enter top text" />
-        </Col>
-        <Col>
-          <Form.Label className="text-center">Bottom Text</Form.Label>
-          <Form.Control type="text" placeholder="Enter bottom text" />
-        </Col>
-      </Row>
-      <Button variant="primary" type="submit" id="submit-button">
-        Get a new meme image 🖼
-      </Button>
-    </Form>
+    <Container>
+      <Form className="form-content">
+        <Row className="mb-3">
+          <Col>
+            <Form.Label className="text-center">Top Text</Form.Label>
+            <Form.Control type="text" placeholder="Enter top text" />
+          </Col>
+          <Col>
+            <Form.Label className="text-center">Bottom Text</Form.Label>
+            <Form.Control type="text" placeholder="Enter bottom text" />
+          </Col>
+        </Row>
+        <Button variant="primary" type="submit" id="submit-button">
+          Get a new meme image 🖼
+        </Button>
+      </Form>
+      <img
+        src="./images/memeimg.png"
+        alt="meme image"
+        className="meme-img"
+      ></img>
+    </Container>
   );
 };
 
